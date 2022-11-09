666da2f37cee031c854bfa8dc093d64566c5b14d
Qingfei You <qingfeiyou@fb.com>
2022-11-09 16:06:49 +0000

    feat(Evaluator): get optimal hands for 7 cards

--- evaluations/EvaluatorOf7Cards.py
+++ evaluations/EvaluatorOf7Cards.py
@@ -1,6 +1,9 @@
+from itertools import combinations
 from typing import List
 
 from cards.Card import Card
+
+from evaluations.EvaluatorOf5Cards import EvaluatorOf5Cards
 
 NUMBER_OF_CARDS = 7
 
@@ -10,49 +13,5 @@ class EvaluatorOf7Cards:
         assert len(set(cards)) == NUMBER_OF_CARDS, f"Must be {NUMBER_OF_CARDS}"
         self.original_cards = cards
 
-    def getOptimalCards(self):
-        pass
-
-    @staticmethod
-    def has_flush(cards: List[Card]) -> bool:
-        groups = {}
-        for card in cards:
-            lst = groups.get(card.genre, [])
-            lst.append(card)
-            groups[card.genre] = lst
-
-        return any(len(val) >= 5 for _, val in groups.items())
-
-    @staticmethod
-    def has_straight(cards: List[Card]) -> bool:
-        numbers = sorted(list(set(c.number.value for c in cards)))
-        n = len(numbers)
-        if n < 5:
-            return False
-
-        def _verify_5_cards(numbers: List[int]) -> bool:
-            assert len(numbers) == 5
-
-            for i in range(len(numbers) - 1):
-                if numbers[i + 1] - numbers[i] != 1:
-                    return False
-            return True
-
-        for i in range(n - 4):
-            if _verify_5_cards(numbers[i : i + 5]):
-                return True
-
-        if numbers[n - 1] == 14:
-            if _verify_5_cards([1] + numbers[0:4]):
-                return True
-        return False
-
-    @staticmethod
-    def has_four_of_kind(cards: List[Card]) -> bool:
-        groups = {}
-        for card in cards:
-            lst = groups.get(card.number, [])
-            lst.append(card)
-            groups[card.number] = lst
-
-        return any(len(val) == 4 for _, val in groups.items())
+    def getOptimalHands(self) -> EvaluatorOf5Cards:
+        return max(EvaluatorOf5Cards(c) for c in combinations(self.original_cards, 5))
--- tests/test_evaluation_7_cards.py
+++ tests/test_evaluation_7_cards.py
@@ -1,6 +1,7 @@
 import unittest
 
 from cards.Card import Card, CardNumber, Genre
+from evaluations.EvaluatorOf5Cards import EvaluatorOf5Cards
 from evaluations.EvaluatorOf7Cards import EvaluatorOf7Cards
 
 
@@ -8,117 +9,8 @@ class TestEvaluationOf7Cards(unittest.TestCase):
     def setUp(self) -> None:
         return super().setUp()
 
-    def test_has_flush(self):
-        # Not a flush
-        stack = [
-            Card(Genre.DIAMOND, CardNumber.QUEEN),
-            Card(Genre.SPADE, CardNumber.ACE),
-            Card(Genre.SPADE, CardNumber.KING),
-            Card(Genre.HEART, CardNumber.EIGHT),
-            Card(Genre.CLUB, CardNumber.JACK),
-            Card(Genre.DIAMOND, CardNumber.NINE),
-            Card(Genre.DIAMOND, CardNumber.ACE),
-        ]
-        self.assertFalse(EvaluatorOf7Cards.has_flush(stack))
-
-        # Five of a genre
-        stack = [
-            Card(Genre.DIAMOND, CardNumber.QUEEN),
-            Card(Genre.DIAMOND, CardNumber.JACK),
-            Card(Genre.DIAMOND, CardNumber.KING),
-            Card(Genre.HEART, CardNumber.EIGHT),
-            Card(Genre.CLUB, CardNumber.JACK),
-            Card(Genre.DIAMOND, CardNumber.NINE),
-            Card(Genre.DIAMOND, CardNumber.ACE),
-        ]
-        self.assertTrue(EvaluatorOf7Cards.has_flush(stack))
-
-        # More than 5 of a genre
-        stack = [
-            Card(Genre.CLUB, CardNumber.QUEEN),
-            Card(Genre.CLUB, CardNumber.JACK),
-            Card(Genre.CLUB, CardNumber.KING),
-            Card(Genre.CLUB, CardNumber.EIGHT),
-            Card(Genre.CLUB, CardNumber.JACK),
-            Card(Genre.CLUB, CardNumber.NINE),
-            Card(Genre.CLUB, CardNumber.ACE),
-        ]
-        self.assertTrue(EvaluatorOf7Cards.has_flush(stack))
-
-    def test_has_straight(self):
-        # 2, 3, 4, 5, 6
-        stack = [
-            Card(Genre.CLUB, CardNumber.TWO),
-            Card(Genre.CLUB, CardNumber.THREE),
-            Card(Genre.HEART, CardNumber.FOUR),
-            Card(Genre.CLUB, CardNumber.FIVE),
-            Card(Genre.SPADE, CardNumber.SIX),
-            Card(Genre.CLUB, CardNumber.NINE),
-            Card(Genre.CLUB, CardNumber.ACE),
-        ]
-        self.assertTrue(EvaluatorOf7Cards.has_straight(stack))
-
-        # 10, J, Q, K, A
-        stack = [
-            Card(Genre.CLUB, CardNumber.TEN),
-            Card(Genre.CLUB, CardNumber.KING),
-            Card(Genre.CLUB, CardNumber.JACK),
-            Card(Genre.HEART, CardNumber.QUEEN),
-            Card(Genre.SPADE, CardNumber.SIX),
-            Card(Genre.CLUB, CardNumber.NINE),
-            Card(Genre.CLUB, CardNumber.ACE),
-        ]
-        self.assertTrue(EvaluatorOf7Cards.has_straight(stack))
-
-        # 1, 2, 3, 4, 5
-        stack = [
-            Card(Genre.CLUB, CardNumber.TWO),
-            Card(Genre.CLUB, CardNumber.THREE),
-            Card(Genre.HEART, CardNumber.FOUR),
-            Card(Genre.CLUB, CardNumber.FIVE),
-            Card(Genre.SPADE, CardNumber.KING),
-            Card(Genre.CLUB, CardNumber.NINE),
-            Card(Genre.CLUB, CardNumber.ACE),
-        ]
-        self.assertTrue(EvaluatorOf7Cards.has_straight(stack))
-
-        # Less than 5 distinct numbers
-        stack = [
-            Card(Genre.CLUB, CardNumber.TWO),
-            Card(Genre.CLUB, CardNumber.THREE),
-            Card(Genre.HEART, CardNumber.THREE),
-            Card(Genre.CLUB, CardNumber.KING),
-            Card(Genre.SPADE, CardNumber.KING),
-            Card(Genre.HEART, CardNumber.KING),
-            Card(Genre.CLUB, CardNumber.ACE),
-        ]
-        self.assertFalse(EvaluatorOf7Cards.has_straight(stack))
-
-        # J, Q, K, A, 2 is not a straight
-        stack = [
-            Card(Genre.CLUB, CardNumber.JACK),
-            Card(Genre.HEART, CardNumber.QUEEN),
-            Card(Genre.CLUB, CardNumber.KING),
-            Card(Genre.SPADE, CardNumber.TWO),
-            Card(Genre.CLUB, CardNumber.NINE),
-            Card(Genre.DIAMOND, CardNumber.NINE),
-            Card(Genre.CLUB, CardNumber.ACE),
-        ]
-        self.assertFalse(EvaluatorOf7Cards.has_straight(stack))
-
-        # No straight
-        stack = [
-            Card(Genre.CLUB, CardNumber.THREE),
-            Card(Genre.HEART, CardNumber.FOUR),
-            Card(Genre.CLUB, CardNumber.FIVE),
-            Card(Genre.SPADE, CardNumber.SIX),
-            Card(Genre.CLUB, CardNumber.NINE),
-            Card(Genre.DIAMOND, CardNumber.NINE),
-            Card(Genre.CLUB, CardNumber.ACE),
-        ]
-        self.assertFalse(EvaluatorOf7Cards.has_straight(stack))
-
-    def test_has_four_of_kind(self):
+    def test_get_optimal_hands(self):
+        # Given
         stack = [
             Card(Genre.CLUB, CardNumber.THREE),
             Card(Genre.HEART, CardNumber.THREE),
@@ -128,15 +20,23 @@ class TestEvaluationOf7Cards(unittest.TestCase):
             Card(Genre.DIAMOND, CardNumber.NINE),
             Card(Genre.CLUB, CardNumber.ACE),
         ]
-        self.assertTrue(EvaluatorOf7Cards.has_four_of_kind(stack))
 
-        stack = [
-            Card(Genre.CLUB, CardNumber.THREE),
-            Card(Genre.HEART, CardNumber.KING),
-            Card(Genre.DIAMOND, CardNumber.THREE),
-            Card(Genre.SPADE, CardNumber.THREE),
-            Card(Genre.CLUB, CardNumber.NINE),
-            Card(Genre.DIAMOND, CardNumber.NINE),
-            Card(Genre.CLUB, CardNumber.ACE),
-        ]
-        self.assertFalse(EvaluatorOf7Cards.has_four_of_kind(stack))
+        # When
+        e = EvaluatorOf7Cards(stack)
+        best = e.getOptimalHands()
+
+        # Then
+        self.assertTrue(
+            best
+            == EvaluatorOf5Cards(
+                [
+                    Card(Genre.CLUB, CardNumber.THREE),
+                    Card(Genre.HEART, CardNumber.THREE),
+                    Card(Genre.DIAMOND, CardNumber.THREE),
+                    Card(Genre.SPADE, CardNumber.THREE),
+                    Card(Genre.CLUB, CardNumber.ACE),
+                ]
+            ),
+            msg="Best hand is quads plus an Ace high card.",
+        )
+        self.assertTrue(set(best.original_cards) <= set(stack), msg="Best hand should be a subset of the 7 cards.")
