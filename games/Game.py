2bda0b26515a65dcfa88cdf980b73b09e11b4bf1
Qingfei You <qingfeiyou@fb.com>
2023-01-09 14:21:33 +0000

    feat(Game): settle showdown

--- games/Game.py
+++ games/Game.py
@@ -25,7 +25,7 @@ class Game:
         self.player_list[position] = player
 
     def get_players_in_the_game(self) -> List:
-        return [ply for (_pos, ply) in sorted(self.player_list.items())]
+        return [ply for (_pos, ply) in sorted(self.player_list.items()) if ply.is_in_game is True]
 
     def start(self) -> None:
         # 1. Preflop
@@ -84,7 +84,7 @@ class Game:
             return
 
         # Showdown
-        _player_to_evaluation = self.showdown(
+        eva_to_players = self.showdown(
             reduce(
                 lambda acc, it: acc + it,
                 map(list, (flop.community_cards, turn.community_cards, river.community_cards)),
@@ -92,6 +92,7 @@ class Game:
             ),
             remaining_players,
         )
+        self.settle_showdown(eva_to_players)
 
     def showdown(self, community_cards: List[Card], remaining_players: List) -> OrderedDict[EvaluatorOf5Cards, List]:
         result: OrderedDict[EvaluatorOf5Cards, List] = OrderedDict()
@@ -105,5 +106,67 @@ class Game:
             else:
                 result[eva] = [player]
 
+        result = {eva: sorted(players, key=lambda p: self.pot.get_chips_for(p)) for eva, players in result.items()}
         result = OrderedDict(sorted(result.items(), key=lambda pair: pair[0], reverse=True))
         return result
+
+    def settle_showdown(self, evaluation_to_players: OrderedDict[EvaluatorOf5Cards, List]) -> int:
+        remaining_players = set(self.player_list.values())
+
+        pot_at_showdown = {player: self.pot.get_chips_for(player) for _pos, player in self.player_list.items()}
+
+        # In case there is remainder
+        surplus = 0
+
+        # iteration is just for the algorithm efficiency testing purpose
+        iteration = 0
+        while len(evaluation_to_players) > 0:
+            eva = next(iter(evaluation_to_players))
+            players = evaluation_to_players.pop(eva)
+
+            remaining_players = remaining_players.difference(players)
+            pot_of_weaker_hands = {
+                player: chip for player, chip in pot_at_showdown.items() if player in remaining_players
+            }
+
+            while len(players) > 0:
+                curr = players[0]
+                bet = pot_at_showdown[curr]
+
+                if bet == 0:
+                    # remove curr player and jump to the next
+                    players.pop(0)
+                    continue
+
+                if sum(pot_at_showdown.values()) == 0:
+                    break
+
+                iteration += 1
+                # Each player from this current tier gets back from self
+                # Ensured for the pot to have sufficient remainings as the player list is sorted by betting chips
+                for peer in players:
+                    peer.money += bet
+                    pot_at_showdown[peer] -= bet
+
+                # All players from this current tier get from people with weaker hands
+                # The chips from people with weaker hands will be split equally
+                total_owe = sum(min(chip, bet) for _other, chip in pot_of_weaker_hands.items())
+                n = len(players)
+                owe, remainder = total_owe // n, total_owe % n
+                surplus += remainder
+
+                # Distribute
+                for peer in players:
+                    peer.money += owe
+
+                # Update chips in the pot for people with weaker hands
+                pot_of_weaker_hands = {other: max(0, chip - bet) for other, chip in pot_of_weaker_hands.items()}
+                pot_at_showdown = {
+                    p: pot_at_showdown[p] for p in set(pot_at_showdown) - set(pot_of_weaker_hands)
+                } | pot_of_weaker_hands
+
+                players.pop(0)
+
+        # All remainder goes to the first person to the left of the dealer
+        self.get_players_in_the_game()[0].money += surplus
+        return iteration
--- tests/GameTest.py
+++ tests/GameTest.py
@@ -1,15 +1,20 @@
+import random
 import unittest
 
 from cards.Card import Card, CardNumber, Genre
 from evaluations.Evaluation import Evaluation
+from evaluations.EvaluatorOf5Cards import EvaluatorOf5Cards
 from games.Deck import Deck
 from games.Game import Game
 from players.Player import ID_generator, Player
 from players.Policy import Policy
+from rounds.Pot import Pot
+from rounds.RoundName import RoundName
 
 
 class GameTest(unittest.TestCase):
     def setUp(self) -> None:
+        unittest.TextTestRunner(verbosity=2)
         return super().setUp()
 
     # --------------------- Player Seats ------------------------
@@ -101,6 +106,9 @@ class GameTest(unittest.TestCase):
 
     def test_game_goes_to_comparison_after_river_if_there_are_more_than_one_player_remained(self):
         # Given
+        # This seed determines the Deck and there for is responsible for the winner.
+        random.seed(1)
+
         game = Game()
         tom = Player("Tom Dwan", ID=ID_generator(), money=50, policy=Policy.ALWAYS_CHECK_OR_FOLD)
         tom.join_game(game, position=3)
@@ -118,7 +126,7 @@ class GameTest(unittest.TestCase):
         self.assertTrue(game.deck.index == 16)
         self.assertTrue(game.pot.compute_total_sum() == 5)
         self.assertTrue(phil.money == 149)
-        self.assertTrue(harry.money == 298)
+        self.assertTrue(harry.money == 303)
         self.assertTrue(tom.money == 50)
         self.assertTrue(anton.money == 998)
 
@@ -147,8 +155,8 @@ class GameTest(unittest.TestCase):
             Card(Genre.SPADE, CardNumber.EIGHT),
         )
         anton.pocket_cards = (
-            Card(Genre.CLUB, CardNumber.THREE),
-            Card(Genre.CLUB, CardNumber.FOUR),
+            Card(Genre.SPADE, CardNumber.THREE),
+            Card(Genre.SPADE, CardNumber.FOUR),
         )
 
         community_cards = [
@@ -166,20 +174,20 @@ class GameTest(unittest.TestCase):
         self.assertTrue(len(evaluation_order) == 4)
         self.assertTrue(
             [eva.get_evaluation() for eva in evaluation_order.keys()]
-            == [Evaluation.FLUSH, Evaluation.STRAIGHT, Evaluation.THREE_OF_A_KIND, Evaluation.TWO_PAIRS]
+            == [Evaluation.FLUSH, Evaluation.FLUSH, Evaluation.THREE_OF_A_KIND, Evaluation.TWO_PAIRS]
         )
         self.assertTrue(list(evaluation_order.values()) == [[tom], [anton], [phil], [harry]])
 
     def test_showdown_with_same_optimal_hands(self):
         # Given
         game = Game()
-        tom = Player("Tom Dwan", ID=ID_generator(), money=50, policy=Policy.ALWAYS_CHECK_OR_FOLD)
-        tom.join_game(game, position=3)
+        tom = Player("Tom Dwan", ID=ID_generator(), money=50, policy=Policy.ALWAYS_CALL)
+        tom.join_game(game, position=4)
         harry = Player("Harry Potter", ID=ID_generator(), money=300, policy=Policy.ALWAYS_CALL)
         harry.join_game(game, position=1)
         anton = Player("Anton Dom", ID=ID_generator(), money=1000, policy=Policy.ALWAYS_CALL)
-        anton.join_game(game, position=4)
-        phil = Player("Phil Ivey", ID=ID_generator(), money=150, policy=Policy.ALWAYS_CHECK_OR_FOLD)
+        anton.join_game(game, position=3)
+        phil = Player("Phil Ivey", ID=ID_generator(), money=150, policy=Policy.ALWAYS_CALL)
         phil.join_game(game, position=0)
 
         phil.pocket_cards = (
@@ -206,9 +214,16 @@ class GameTest(unittest.TestCase):
             Card(Genre.HEART, CardNumber.ACE),
             Card(Genre.SPADE, CardNumber.FIVE),
         ]
+        game.pot = Pot()
+        game.pot.roundpots = {
+            RoundName.PREFLOP: {phil: 10, harry: 10, tom: 10, anton: 10},
+            RoundName.FLOP: {phil: 10, harry: 10, tom: 10, anton: 10},
+            RoundName.TURN: {phil: 0, harry: 0, tom: 0, anton: 0},
+            RoundName.RIVER: {phil: 50, harry: 50, tom: 30, anton: 50},
+        }
 
         # When
-        evaluation_order = game.showdown(community_cards, [phil, harry, tom, anton])
+        evaluation_order = game.showdown(community_cards, game.get_players_in_the_game())
 
         # Then
         self.assertTrue(len(evaluation_order) == 3)
@@ -217,3 +232,294 @@ class GameTest(unittest.TestCase):
             == [Evaluation.STRAIGHT, Evaluation.THREE_OF_A_KIND, Evaluation.TWO_PAIRS]
         )
         self.assertTrue(list(evaluation_order.values()) == [[tom, anton], [phil], [harry]])
+
+    def test_settle_showdown_with_all_in_and_side_pots(self) -> None:
+        # Given
+        game = Game()
+
+        patrick = Player("Patrick Antonius", ID=ID_generator(), money=1_000, policy=Policy.ALWAYS_CALL)
+        patrick.join_game(game, position=2)
+        allen = Player("Allen Baggio", ID=ID_generator(), money=1_000, policy=Policy.ALWAYS_CALL)
+        allen.join_game(game, position=5)
+        tom = Player("Tom Dwan", ID=ID_generator(), money=65, policy=Policy.ALWAYS_CALL)
+        tom.join_game(game, position=4)
+        harry = Player("Harry Potter", ID=ID_generator(), money=135, policy=Policy.ALWAYS_CALL)
+        harry.join_game(game, position=1)
+        anton = Player("Anton Dom", ID=ID_generator(), money=1_000, policy=Policy.ALWAYS_CALL)
+        anton.join_game(game, position=3)
+        phil = Player("Phil Ivey", ID=ID_generator(), money=85, policy=Policy.ALWAYS_CALL)
+        phil.join_game(game, position=0)
+
+        # Simulate betting
+        game.pot = Pot()
+        game.pot.roundpots = {
+            RoundName.PREFLOP: {phil: 10, harry: 10, patrick: 10, anton: 10, tom: 10, allen: 10},
+            RoundName.FLOP: {phil: 25, harry: 25, patrick: 0, anton: 25, tom: 25, allen: 0},
+            RoundName.TURN: {phil: 0, harry: 0, patrick: 0, anton: 0, tom: 0, allen: 0},
+            RoundName.RIVER: {phil: 50, harry: 100, patrick: 0, anton: 200, tom: 30, allen: 0},
+        }
+        for p in game.get_players_in_the_game():
+            p.money -= game.pot.get_chips_for(p)
+
+        # Simulate hands
+        eva_to_players = {
+            EvaluatorOf5Cards(
+                [
+                    Card(Genre.CLUB, CardNumber.ACE),
+                    Card(Genre.SPADE, CardNumber.ACE),
+                    Card(Genre.DIAMOND, CardNumber.QUEEN),
+                    Card(Genre.DIAMOND, CardNumber.ACE),
+                    Card(Genre.HEART, CardNumber.ACE),
+                ]
+            ): [tom],
+            EvaluatorOf5Cards(
+                [
+                    Card(Genre.CLUB, CardNumber.ACE),
+                    Card(Genre.SPADE, CardNumber.ACE),
+                    Card(Genre.CLUB, CardNumber.KING),
+                    Card(Genre.HEART, CardNumber.KING),
+                    Card(Genre.HEART, CardNumber.JACK),
+                ]
+            ): [phil, harry],
+            EvaluatorOf5Cards(
+                [
+                    Card(Genre.CLUB, CardNumber.ACE),
+                    Card(Genre.SPADE, CardNumber.ACE),
+                    Card(Genre.DIAMOND, CardNumber.QUEEN),
+                    Card(Genre.CLUB, CardNumber.KING),
+                    Card(Genre.HEART, CardNumber.QUEEN),
+                ]
+            ): [anton],
+        }
+
+        # When
+        iteration = game.settle_showdown(eva_to_players)
+
+        # Then
+        self.assertTrue(iteration == 4, "One iteration per player.")
+        self.assertTrue(tom.money == 65 + 65 + 10 + 65 + 65 + 10)
+        self.assertTrue(phil.money == 20 + 20 // 2)
+        self.assertTrue(harry.money == (20 + 20 // 2) + (50 + 50 // 1))
+        self.assertTrue(anton.money == 865)
+        self.assertTrue(patrick.money == 990)
+        self.assertTrue(allen.money == 990)
+
+    def test_settle_showdown_with_remainders(self) -> None:
+        # Given
+        game = Game()
+
+        patrick = Player("Patrick Antonius", ID=ID_generator(), money=1_000, policy=Policy.ALWAYS_CALL)
+        patrick.join_game(game, position=2)
+        allen = Player("Allen Baggio", ID=ID_generator(), money=1_000, policy=Policy.ALWAYS_CALL)
+        allen.join_game(game, position=5)
+        tom = Player("Tom Dwan", ID=ID_generator(), money=65, policy=Policy.ALWAYS_CALL)
+        tom.join_game(game, position=4)
+        harry = Player("Harry Potter", ID=ID_generator(), money=135, policy=Policy.ALWAYS_CALL)
+        harry.join_game(game, position=1)
+        anton = Player("Anton Dom", ID=ID_generator(), money=1_000, policy=Policy.ALWAYS_CALL)
+        anton.join_game(game, position=3)
+        phil = Player("Phil Ivey", ID=ID_generator(), money=85, policy=Policy.ALWAYS_CALL)
+        phil.join_game(game, position=0)
+
+        # Simulate betting
+        game.pot = Pot()
+        game.pot.roundpots = {
+            RoundName.PREFLOP: {phil: 10, harry: 10, patrick: 10, anton: 10, tom: 10, allen: 10},
+            RoundName.FLOP: {phil: 25, harry: 25, patrick: 0, anton: 25, tom: 25, allen: 0},
+            RoundName.TURN: {phil: 0, harry: 0, patrick: 0, anton: 0, tom: 0, allen: 0},
+            RoundName.RIVER: {phil: 50, harry: 100, patrick: 0, anton: 200, tom: 30, allen: 0},
+        }
+        for p in game.get_players_in_the_game():
+            p.money -= game.pot.get_chips_for(p)
+
+        # Simulate hands
+        eva_to_players = {
+            EvaluatorOf5Cards(
+                [
+                    Card(Genre.CLUB, CardNumber.ACE),
+                    Card(Genre.SPADE, CardNumber.KING),
+                    Card(Genre.DIAMOND, CardNumber.QUEEN),
+                    Card(Genre.DIAMOND, CardNumber.JACK),
+                    Card(Genre.HEART, CardNumber.TEN),
+                ]
+            ): [tom, phil, harry],
+            EvaluatorOf5Cards(
+                [
+                    Card(Genre.CLUB, CardNumber.ACE),
+                    Card(Genre.SPADE, CardNumber.ACE),
+                    Card(Genre.DIAMOND, CardNumber.QUEEN),
+                    Card(Genre.CLUB, CardNumber.KING),
+                    Card(Genre.HEART, CardNumber.QUEEN),
+                ]
+            ): [anton],
+        }
+
+        # When
+        iteration = game.settle_showdown(eva_to_players)
+
+        # Then
+        self.assertTrue(iteration == 4, "One iteration per player.")
+        self.assertTrue(
+            tom.money == 65 + (0 + 0 + 10 + 65 + 0 + 10) // 3,
+            "Tom bets the smallest and goes with the 1st side pot to share with 2 other players.",
+        )
+        self.assertTrue(
+            harry.money
+            == 65
+            + (0 + 0 + 10 + 65 + 0 + 10) // 3
+            + 20
+            + (0 + 0 + 0 + 20 + 0 + 0) // 2
+            + 50
+            + (0 + 0 + 0 + 50 + 0 + 0) // 1,
+            "Harry bets 3rd smallest and goes with 1st, 2nd, 3rd side pot of 3, 2, 1 players respectively.",
+        )
+        self.assertTrue(anton.money == 865)
+        self.assertTrue(
+            phil.money
+            == 65
+            + (0 + 0 + 10 + 65 + 0 + 10) // 3
+            + 20
+            + (0 + 0 + 0 + 20 + 0 + 0) // 2
+            + (0 + 0 + 10 + 65 + 0 + 10) % 3  # Don't forget remainders
+            + (0 + 0 + 0 + 20 + 0 + 0) % 2
+            + (0 + 0 + 0 + 50 + 0 + 0) % 1,
+            "Phil bets 2nd smallest and goes with 1st, 2nd side pot of 3 and 2 players respectively.",
+        )
+        self.assertTrue(patrick.money == 990)
+        self.assertTrue(allen.money == 990)
+
+    def test_settle_showdown_with_side_pots_with_one_pot_where_players_put_the_same_bet(self) -> None:
+        # Given
+        game = Game()
+
+        patrick = Player("Patrick Antonius", ID=ID_generator(), money=1_000, policy=Policy.ALWAYS_CALL)
+        patrick.join_game(game, position=2)
+        allen = Player("Allen Baggio", ID=ID_generator(), money=1_000, policy=Policy.ALWAYS_CALL)
+        allen.join_game(game, position=5)
+        tom = Player("Tom Dwan", ID=ID_generator(), money=65, policy=Policy.ALWAYS_CALL)
+        tom.join_game(game, position=4)
+        harry = Player("Harry Potter", ID=ID_generator(), money=85, policy=Policy.ALWAYS_CALL)
+        harry.join_game(game, position=1)
+        anton = Player("Anton Dom", ID=ID_generator(), money=1_000, policy=Policy.ALWAYS_CALL)
+        anton.join_game(game, position=3)
+        phil = Player("Phil Ivey", ID=ID_generator(), money=85, policy=Policy.ALWAYS_CALL)
+        phil.join_game(game, position=0)
+
+        # Simulate betting
+        game.pot = Pot()
+        game.pot.roundpots = {
+            RoundName.PREFLOP: {phil: 10, harry: 10, patrick: 10, anton: 10, tom: 10, allen: 10},
+            RoundName.FLOP: {phil: 25, harry: 25, patrick: 0, anton: 25, tom: 25, allen: 0},
+            RoundName.TURN: {phil: 0, harry: 0, patrick: 0, anton: 0, tom: 0, allen: 0},
+            RoundName.RIVER: {phil: 50, harry: 50, patrick: 0, anton: 200, tom: 30, allen: 0},
+        }  # Phil and Harry bet the same
+        for p in game.get_players_in_the_game():
+            p.money -= game.pot.get_chips_for(p)
+
+        # Simulate hands
+        eva_to_players = {
+            EvaluatorOf5Cards(
+                [
+                    Card(Genre.CLUB, CardNumber.ACE),
+                    Card(Genre.SPADE, CardNumber.ACE),
+                    Card(Genre.DIAMOND, CardNumber.QUEEN),
+                    Card(Genre.DIAMOND, CardNumber.ACE),
+                    Card(Genre.HEART, CardNumber.ACE),
+                ]
+            ): [tom],
+            EvaluatorOf5Cards(
+                [
+                    Card(Genre.CLUB, CardNumber.ACE),
+                    Card(Genre.SPADE, CardNumber.ACE),
+                    Card(Genre.CLUB, CardNumber.KING),
+                    Card(Genre.HEART, CardNumber.KING),
+                    Card(Genre.HEART, CardNumber.JACK),
+                ]
+            ): [
+                phil,
+                harry,
+            ],  # Phil and Harry are in the same pot
+            EvaluatorOf5Cards(
+                [
+                    Card(Genre.CLUB, CardNumber.ACE),
+                    Card(Genre.SPADE, CardNumber.ACE),
+                    Card(Genre.DIAMOND, CardNumber.QUEEN),
+                    Card(Genre.CLUB, CardNumber.KING),
+                    Card(Genre.HEART, CardNumber.QUEEN),
+                ]
+            ): [anton],
+        }
+
+        # When
+        iteration = game.settle_showdown(eva_to_players)
+
+        # Then
+        self.assertTrue(iteration == 3, "2nd side pot has two players with same bets and should have 1 iteration.")
+        self.assertTrue(tom.money == 65 + 65 + 10 + 65 + 65 + 10, "1st side pot")
+        self.assertTrue(phil.money == 20 + 20 // 2, "2nd side pot.")
+        self.assertTrue(harry.money == 20 + 20 // 2, "2nd side pot.")
+        self.assertTrue(anton.money == 915)
+        self.assertTrue(patrick.money == 990)
+        self.assertTrue(allen.money == 990)
+
+    def test_settle_showdown_where_players_with_the_best_hands_bet_the_most(self) -> None:
+        # Given
+        game = Game()
+
+        patrick = Player("Patrick Antonius", ID=ID_generator(), money=1_000, policy=Policy.ALWAYS_CALL)
+        patrick.join_game(game, position=2)
+        allen = Player("Allen Baggio", ID=ID_generator(), money=1_000, policy=Policy.ALWAYS_CALL)
+        allen.join_game(game, position=5)
+        tom = Player("Tom Dwan", ID=ID_generator(), money=65, policy=Policy.ALWAYS_CALL)
+        tom.join_game(game, position=4)
+        harry = Player("Harry Potter", ID=ID_generator(), money=135, policy=Policy.ALWAYS_CALL)
+        harry.join_game(game, position=1)
+        anton = Player("Anton Dom", ID=ID_generator(), money=1_000, policy=Policy.ALWAYS_CALL)
+        anton.join_game(game, position=3)
+        phil = Player("Phil Ivey", ID=ID_generator(), money=85, policy=Policy.ALWAYS_CALL)
+        phil.join_game(game, position=0)
+
+        # Simulate betting
+        game.pot = Pot()
+        game.pot.roundpots = {
+            RoundName.PREFLOP: {phil: 10, harry: 10, patrick: 10, anton: 10, tom: 10, allen: 10},
+            RoundName.FLOP: {phil: 25, harry: 25, patrick: 0, anton: 25, tom: 25, allen: 0},
+            RoundName.TURN: {phil: 0, harry: 0, patrick: 0, anton: 0, tom: 0, allen: 0},
+            RoundName.RIVER: {phil: 50, harry: 100, patrick: 0, anton: 200, tom: 30, allen: 0},
+        }  # Phil and Harry bet the same
+        for p in game.get_players_in_the_game():
+            p.money -= game.pot.get_chips_for(p)
+
+        # Simulate hands
+        eva_to_players = {
+            EvaluatorOf5Cards(
+                [
+                    Card(Genre.CLUB, CardNumber.ACE),
+                    Card(Genre.SPADE, CardNumber.ACE),
+                    Card(Genre.DIAMOND, CardNumber.QUEEN),
+                    Card(Genre.DIAMOND, CardNumber.ACE),
+                    Card(Genre.HEART, CardNumber.ACE),
+                ]
+            ): [anton],
+            EvaluatorOf5Cards(
+                [
+                    Card(Genre.CLUB, CardNumber.ACE),
+                    Card(Genre.SPADE, CardNumber.ACE),
+                    Card(Genre.DIAMOND, CardNumber.QUEEN),
+                    Card(Genre.CLUB, CardNumber.KING),
+                    Card(Genre.HEART, CardNumber.QUEEN),
+                ]
+            ): [tom, phil, harry],
+        }
+
+        # When
+        iteration = game.settle_showdown(eva_to_players)
+
+        # Then
+        self.assertTrue(iteration == 1, "Should finish calculation once the 1st side pot is settled.")
+        self.assertTrue(anton.money == 85 + 135 + 10 + 1_000 + 65 + 10, "1st side pot")
+        self.assertTrue(tom.money == 0, "2nd side pot")
+        self.assertTrue(phil.money == 0, "2nd side pot.")
+        self.assertTrue(harry.money == 0, "2nd side pot.")
+        self.assertTrue(patrick.money == 990)
+        self.assertTrue(allen.money == 990)
